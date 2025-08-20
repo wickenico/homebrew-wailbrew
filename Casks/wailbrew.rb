f027fcb7c5aff9ff4a6270a80dd20beadb38ae8b
Nico Wickersheim <nico.wickersheim@sevdesk.de>
2025-08-20 22:57:12 +0200

    Fix wailbrew.rb

--- Casks/wailbrew.rb
+++ Casks/wailbrew.rb
@@ -1,12 +1,14 @@
 cask "wailbrew" do
   version "0.6.0"
-  sha256 "ec917addb6aff24129cdc40818c0a75ee01523ebfee550f4db8d5401ada71311p"
+  sha256 "b82ca685419d5edeb7fd8f3973d0c104dfcf891a9d08992d72bb929c5adac91d"
 
-  url "https://github.com/wickenico/WailBrew/releases/download/v#{version}/wailbrew-v#{version}.zip",
-      verified: "github.com/wickenico/WailBrew/"
+  url "https://github.com/wickenico/WailBrew/releases/download/v#{version}/wailbrew-v#{version}.zip"
   name "WailBrew"
   desc "Manage Homebrew packages with a UI"
   homepage "https://github.com/wickenico/WailBrew"
+
+  depends_on macos: ">= :ventura"
+  depends_on arch: :arm64
 
   app "WailBrew.app"
 
@@ -16,4 +18,3 @@ cask "wailbrew" do
     "~/Library/Saved Application State/dev.wailbrew.savedState",
   ]
 end
-
