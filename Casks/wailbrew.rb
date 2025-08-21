30bcbc8ec101df670145d657f1073227ad7435f8
Nico Wickersheim <nico.wickersheim@sevdesk.de>
2025-08-21 15:02:32 +0200

    Bump wailbrew to 0.7.0

--- Casks/wailbrew.rb
+++ Casks/wailbrew.rb
@@ -1,6 +1,6 @@
 cask "wailbrew" do
-  version "0.6.1"
-  sha256 "512b6d57ddf16cd5265db132d5a4b44187782dbe3795d7fb3491648f802cf6e6"
+  version "0.7.0"
+  sha256 "4a685a7eaa1c0b565eaa0f6916835eb57c45a20c268c3e94af2a8f695b1bfdde"
 
   url "https://github.com/wickenico/WailBrew/releases/download/v#{version}/wailbrew-v#{version}.zip"
   name "WailBrew"
