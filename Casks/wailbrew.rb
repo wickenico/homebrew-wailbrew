fdfe02740de5ca6607d8d238ee1e340cc680666b
Nico Wickersheim <nico.wickersheim@sevdesk.de>
2025-10-15 21:10:36 +0200

    Bump wailbrew to 0.7.11 with extended version support

--- Casks/wailbrew.rb
+++ Casks/wailbrew.rb
@@ -1,13 +1,21 @@
 cask "wailbrew" do
-  version "0.7.10"
-  sha256 "4119e06b432d4ea02d0140cca409220d571f8967099bafc1fcef2c6f84f9af3c"
+  version "0.7.11"
+  sha256 "17dcf4fdbce5babf575116d79ce71a1a31a7d8c351dd60c0ed5a763ca84d875b"
 
   url "https://github.com/wickenico/WailBrew/releases/download/v#{version}/wailbrew-v#{version}.zip"
   name "WailBrew"
   desc "Manage Homebrew packages with a UI"
   homepage "https://github.com/wickenico/WailBrew"
 
-  depends_on macos: ">= :ventura"
+  on_intel do
+    # Intel Mojave (10.14)
+    depends_on macos: ">= :mojave"
+  end
+
+  on_arm do
+    # Apple Silicon Big Sur (11)
+    depends_on macos: ">= :big_sur"
+  end
 
   app "WailBrew.app"
 
