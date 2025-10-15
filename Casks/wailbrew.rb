00bf7e323aa14b3929eb81fd44bf0b532744fda1
Nico Wickersheim <nico.wickersheim@sevdesk.de>
2025-10-15 21:21:46 +0200

    Relocate metadata in wailbrew.rb for consistency

--- Casks/wailbrew.rb
+++ Casks/wailbrew.rb
@@ -2,17 +2,17 @@ cask "wailbrew" do
   version "0.7.11"
   sha256 "17dcf4fdbce5babf575116d79ce71a1a31a7d8c351dd60c0ed5a763ca84d875b"
 
+  on_arm do
+    depends_on macos: ">= :big_sur"
+  end
+  on_intel do
+    depends_on macos: ">= :mojave"
+  end
+
   url "https://github.com/wickenico/WailBrew/releases/download/v#{version}/wailbrew-v#{version}.zip"
   name "WailBrew"
   desc "Manage Homebrew packages with a UI"
   homepage "https://github.com/wickenico/WailBrew"
-
-  on_intel do
-    depends_on macos: ">= :mojave"
-  end
-  on_arm do
-    depends_on macos: ">= :big_sur"
-  end
 
   app "WailBrew.app"
 
