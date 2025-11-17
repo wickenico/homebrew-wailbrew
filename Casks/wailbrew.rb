7cb5f56f5f78ef7476d20c56f852c975fc64cf87
Nico Wickersheim <nico.wickersheim@sevdesk.de>
2025-11-17 23:47:37 +0100

    fix depends on

--- Casks/wailbrew.rb
+++ Casks/wailbrew.rb
@@ -2,17 +2,12 @@ cask "wailbrew" do
   version "0.8.1"
   sha256 "8f8b8ece7538f5d1bb782a25f63972fe5ae548e5179c70f772c96f50b5537688"
 
-  on_arm do
-    depends_on macos: ">= :big_sur"
-  end
-  on_intel do
-    depends_on macos: ">= :big_sur"
-  end
-
   url "https://github.com/wickenico/WailBrew/releases/download/v#{version}/wailbrew-v#{version}.zip"
   name "WailBrew"
   desc "Manage Homebrew packages with a UI"
   homepage "https://github.com/wickenico/WailBrew"
+
+  depends_on macos: ">= :big_sur"
 
   app "WailBrew.app"
 
